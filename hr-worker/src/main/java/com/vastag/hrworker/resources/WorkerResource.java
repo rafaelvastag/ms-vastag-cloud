defdf8004e91ab269538a82620da5e525fdfc6ae
Rafael Vastag <rgvastag@gmail.com>
2021-04-03 23:28:15 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/vastag/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/vastag/hrworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +18,19 @@ import com.vastag.hrworker.dto.WorkerDTO;
 import com.vastag.hrworker.service.WorkerService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(value = "/workers")
 @RequiredArgsConstructor
+@Slf4j
 public class WorkerResource {
 
 	private final WorkerService service;
 	private final ModelMapper modelMapper;
+	
+	@Autowired
+	private Environment env;
 
 	@GetMapping
 	public ResponseEntity<List<WorkerDTO>> findAll() {
@@ -34,6 +41,9 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<WorkerDTO> findByWorkerId(@PathVariable(name = "id") Long id) {
+		
+		log.info("PORT = " + env.getProperty("local.server.port"));
+		
 		WorkerDTO worker = service.findById(id).map(entity -> modelMapper.map(entity, WorkerDTO.class))
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		return ResponseEntity.ok(worker);
