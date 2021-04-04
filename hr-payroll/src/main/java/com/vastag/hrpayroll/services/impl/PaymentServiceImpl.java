e61aeaa07bf978241255e0e52b2997e698836320
Rafael Vastag <rgvastag@gmail.com>
2021-04-03 23:06:21 -0300

    feign client

--- hr-payroll/src/main/java/com/vastag/hrpayroll/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/com/vastag/hrpayroll/services/impl/PaymentServiceImpl.java
@@ -1,15 +1,12 @@
 package com.vastag.hrpayroll.services.impl;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.vastag.hrpayroll.dto.WorkerDTO;
 import com.vastag.hrpayroll.entities.Payment;
+import com.vastag.hrpayroll.feignclients.WorkerFeignClient;
 import com.vastag.hrpayroll.services.PaymentService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,17 +15,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
-	private final RestTemplate restTemplate;
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	private final WorkerFeignClient workerFeign;
 
 	@Override
 	public Optional<Payment> getPayment(Long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<String, String>();
-		uriVariables.put("id", "" + workerId);
 
-		WorkerDTO worker = restTemplate.getForObject(workerHost + "/workers/{id}", WorkerDTO.class, uriVariables);
+		WorkerDTO worker = workerFeign.findByWorkerId(workerId).getBody();
 
 		return Optional
 				.of(Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build());
