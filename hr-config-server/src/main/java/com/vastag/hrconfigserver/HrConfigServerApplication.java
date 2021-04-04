6e584f7f05535ac87e8ea8304f96564a9837ba46
Rafael Vastag <rgvastag@gmail.com>
2021-04-04 17:18:17 -0300

    hr-config-server access git private

--- hr-config-server/src/main/java/com/vastag/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/vastag/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.vastag.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,7 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
