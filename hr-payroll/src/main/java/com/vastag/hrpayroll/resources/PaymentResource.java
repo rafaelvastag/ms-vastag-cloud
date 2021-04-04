3224b7331ef57d0f0d38cd16e140b73802b7ff62
Rafael Vastag <rgvastag@gmail.com>
2021-04-04 09:48:46 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/vastag/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/vastag/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.vastag.hrpayroll.dto.PaymentDTO;
 import com.vastag.hrpayroll.services.PaymentService;
 
@@ -22,11 +23,18 @@ public class PaymentResource {
 	private final PaymentService service;
 	private final ModelMapper modelMapper;
 
+	@HystrixCommand(fallbackMethod = "getPaymentFallback")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<PaymentDTO> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		return ResponseEntity
 				.ok(service.getPayment(workerId, days).map(payment -> modelMapper.map(payment, PaymentDTO.class))
 						.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+	}
+
+	public ResponseEntity<PaymentDTO> getPaymentFallback(Long workerId, Integer days) {
+		return ResponseEntity.ok(PaymentDTO.builder()
+				.name("DEFAULT_WORKER").dailyIncome(100.0).days(30)
+				.build());
 	}
 
 }
