c47c873196cbc9a2bd3a601391d2e0f6125169c4
Rafael Vastag <rgvastag@gmail.com>
2021-04-04 17:00:42 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/vastag/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/vastag/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,14 +25,24 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(value = "/workers")
 @RequiredArgsConstructor
+@RefreshScope
 @Slf4j
 public class WorkerResource {
 
 	private final WorkerService service;
 	private final ModelMapper modelMapper;
-	
+
 	@Autowired
 	private Environment env;
+
+	@Value("${test.config}")
+	private String externalConfig;
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<List<WorkerDTO>> findConfigs() {
+		log.info(externalConfig);
+		return ResponseEntity.noContent().build();
+	}
 
 	@GetMapping
 	public ResponseEntity<List<WorkerDTO>> findAll() {
@@ -41,12 +53,12 @@ public class WorkerResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<WorkerDTO> findByWorkerId(@PathVariable(name = "id") Long id) {
-		
+
 		log.info("PORT = " + env.getProperty("local.server.port"));
-		
+
 		WorkerDTO worker = service.findById(id).map(entity -> modelMapper.map(entity, WorkerDTO.class))
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-		
+
 		return ResponseEntity.ok(worker);
 	}
 
