350ab87f11f50c81625ba0d2b0f46fb450065450
Rafael Vastag <rgvastag@gmail.com>
2021-04-05 10:42:30 -0300

    Authentication

--- hr-oauth/src/main/java/com/vastag/hroauth/dto/UserDTO.java
+++ hr-oauth/src/main/java/com/vastag/hroauth/dto/UserDTO.java
@@ -1,9 +1,13 @@
 package com.vastag.hroauth.dto;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import com.vastag.hroauth.entities.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,11 +18,44 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDTO {
+public class UserDTO implements UserDetails {
 
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
 	private Set<RoleDTO> roles = new HashSet<RoleDTO>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles
+				.stream()
+				.map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
--- hr-oauth/src/main/java/com/vastag/hroauth/services/impl/UserServiceImpl.java
+++ hr-oauth/src/main/java/com/vastag/hroauth/services/impl/UserServiceImpl.java
@@ -3,14 +3,20 @@ package com.vastag.hroauth.services.impl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.vastag.hroauth.dto.UserDTO;
 import com.vastag.hroauth.feignclients.UserFeignClient;
 import com.vastag.hroauth.services.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
-public class UserServiceImpl implements UserService {
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService {
 
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -18,5 +24,15 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public Optional<UserDTO> findByEmail(String email) {
 		return Optional.of(userFeignClient.findByEmail(email).getBody());
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserDTO user = userFeignClient.findByEmail(email).getBody();
+		if (null == user) {
+			log.error("Email not found: " + email);
+			throw new UsernameNotFoundException("Email" + email + "not found");
+		}
+		return user;
 	}
 }
